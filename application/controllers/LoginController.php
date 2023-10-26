b052a4f60907ff98a1ad4fc558f891265ce857db
Samuel Prasetyo <sprasetyo0511@gmail.com>
2023-10-26 14:14:15 +0700

    Memperbaiki beberapa error

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class PagesController extends CI_Controller{
+class LoginController extends CI_Controller{
 
     public function __construct()
 	{
@@ -13,7 +13,9 @@ class PagesController extends CI_Controller{
 
 
     public function index() {
+		$this->load->view('layout/header');
         $this->load->view('auth/login');
+		$this->load->view('layout/footer');
     }
 
 
