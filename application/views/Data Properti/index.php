259fab327076ac9e471dd7d8a9589edb295ccb31
MDendiPurwanto <201410001@unma.ac.id>
2023-10-27 08:18:07 +0700

    Update Properti page

--- application/views/Data Properti/index.php
+++ application/views/Data Properti/index.php
@@ -44,8 +44,30 @@
                                 </p>
                                 </div>
                                 <hr class="fw-bold border-1">
-                                <a href="<?= site_url('DetailadminController')?>" class="btn btn-icon icon-left btn-info"><i class="fas fa-info-circle"></i> Info</a>
-                                <a href="<?= site_url('EditDataController')?>" class="btn btn-icon icon-left btn-primary"><i class="far fa-edit"></i> Edit</a>
+                                <a href="<?= site_url('DataPropertiController/detailadmin')?>" class="btn btn-icon icon-left btn-info"><i class="fas fa-info-circle"></i> Info</a>
+                                <a href="<?= site_url('DataPropertiController/editdata')?>" class="btn btn-icon icon-left btn-primary"><i class="far fa-edit"></i> Edit</a>
+                                <button class="btn btn-icon icon-left btn-danger"><i class="fas fa-times"></i> Delete</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card card-info">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <img src="<?= base_url('assets/');?>template/img/995.jpg" width="300" height="200">
+                                </div>
+                                <h6 class="card-title fw-bold mt-3">Rose House</h6>
+                                <div class="d-flex justify-content-between align-items-center">
+                                <p class="card-text text-muted mb-1 font-sm">
+                                    <i class="fas fa-donate"></i> <span class="price">RP.90000000</span>
+                                </p>
+                                <p class="card-text text-muted font-sm">
+                                    <i class="fas fa-map-marker-alt me-1"></i> <span class="location">Jakarta Selatan</span>
+                                </p>
+                                </div>
+                                <hr class="fw-bold border-1">
+                                <a href="<?= site_url('DataPropertiController/detailadmin')?>" class="btn btn-icon icon-left btn-info"><i class="fas fa-info-circle"></i> Info</a>
+                                <a href="<?= site_url('DataPropertiController/editdata')?>" class="btn btn-icon icon-left btn-primary"><i class="far fa-edit"></i> Edit</a> 
                                 <button class="btn btn-icon icon-left btn-danger"><i class="fas fa-times"></i> Delete</button>
                             </div>
                         </div>
@@ -88,8 +110,8 @@
                                 </p>
                                 </div>
                                 <hr class="fw-bold border-1">
-                                <a href="<?= site_url('DetailadminController')?>" class="btn btn-icon icon-left btn-info"><i class="fas fa-info-circle"></i> Info</a>
-                                <a href="<?= site_url('EditDataController')?>" class="btn btn-icon icon-left btn-primary"><i class="far fa-edit"></i> Edit</a>
+                                <a href="<?= site_url('DataPropertiController/detailadmin')?>" class="btn btn-icon icon-left btn-info"><i class="fas fa-info-circle"></i> Info</a>
+                                <a href="<?= site_url('DataPropertiController/editdata')?>" class="btn btn-icon icon-left btn-primary"><i class="far fa-edit"></i> Edit</a>
                                 <button class="btn btn-icon icon-left btn-danger"><i class="fas fa-times"></i> Delete</button>
                             </div>
                         </div>
@@ -110,30 +132,8 @@
                                 </p>
                                 </div>
                                 <hr class="fw-bold border-1">
-                                <a href="<?= site_url('DetailadminController')?>" class="btn btn-icon icon-left btn-info"><i class="fas fa-info-circle"></i> Info</a>
-                                <a href="<?= site_url('EditDataController')?>" class="btn btn-icon icon-left btn-primary"><i class="far fa-edit"></i> Edit</a>
-                                <button class="btn btn-icon icon-left btn-danger"><i class="fas fa-times"></i> Delete</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card card-info">
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img src="<?= base_url('assets/');?>template/img/995.jpg" width="300" height="200">
-                                </div>
-                                <h6 class="card-title fw-bold mt-3">Rose House</h6>
-                                <div class="d-flex justify-content-between align-items-center">
-                                <p class="card-text text-muted mb-1 font-sm">
-                                    <i class="fas fa-donate"></i> <span class="price">RP.90000000</span>
-                                </p>
-                                <p class="card-text text-muted font-sm">
-                                    <i class="fas fa-map-marker-alt me-1"></i> <span class="location">Jakarta Selatan</span>
-                                </p>
-                                </div>
-                                <hr class="fw-bold border-1">
-                                <a href="<?= site_url('DetailadminController')?>" class="btn btn-icon icon-left btn-info"><i class="fas fa-info-circle"></i> Info</a>
-                                <a href="<?= site_url('EditDataController')?>" class="btn btn-icon icon-left btn-primary"><i class="far fa-edit"></i> Edit</a>
+                                <a href="<?= site_url('DataPropertiController/detailadmin')?>" class="btn btn-icon icon-left btn-info"><i class="fas fa-info-circle"></i> Info</a>
+                                <a href="<?= site_url('DataPropertiController/editdata')?>" class="btn btn-icon icon-left btn-primary"><i class="far fa-edit"></i> Edit</a>
                                 <button class="btn btn-icon icon-left btn-danger"><i class="fas fa-times"></i> Delete</button>
                             </div>
                         </div>
