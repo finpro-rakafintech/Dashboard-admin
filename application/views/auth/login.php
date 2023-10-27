45a9c44877fe81283c1e6cdc7641779d798355d3
MDendiPurwanto <201410001@unma.ac.id>
2023-10-27 10:03:13 +0700

    Update Login Page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -6,13 +6,13 @@
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                     <div class="card card-primary">
                         <div class="card-header">
-                            <h4>Login</h4>
+                            <h4>Login Administator</h4>
                         </div>
                         <div class="card-body">
                             <form method="POST" action="#" class="needs-validation" novalidate="">
                                 <div class="form-group">
                                     <label for="email">Email</label>
-                                    <input id="email" type="email" class="form-control" name="email" tabindex="1" required="" autofocus="">
+                                    <input id="email" type="email" class="form-control" name="email" placeholder="info@rakafintech.com" tabindex="1" required="" autofocus="">
                                     <div class="invalid-feedback">
                                         Please fill in your email
                                     </div>
@@ -26,7 +26,7 @@
                                             </a>
                                         </div>
                                     </div>
-                                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required="">
+                                    <input id="password" type="password" class="form-control" placeholder="**********" name="password" tabindex="2" required="">
                                     <div class="invalid-feedback">
                                         please fill in your password
                                     </div>
@@ -46,23 +46,21 @@
                             <div class="text-center mt-4 mb-3">
                                 <div class="text-job text-muted">Login With Social</div>
                             </div>
-                            <div class="row sm-gutters">
+                            <div class="row sm-gutters  ">
                                 <div class="col-6">
-                                    <a class="btn btn-block btn-social btn-facebook">
+                                    <a href="" class="btn  btn-info btn-facebook">
                                         <span class="fab fa-facebook"></span> Facebook
                                     </a>
                                 </div>
-                                <div class="col-6">
-                                    <a class="btn btn-block btn-social btn-twitter">
+                                <div class="col-5">
+                                    <a href="" class="btn btn-light  btn-twitter">
                                         <span class="fab fa-twitter"></span> Twitter
                                     </a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="mt-5 text-muted text-center">
-                        Don't have an account? <a href="auth-register.html">Create One</a>
-                    </div>
+                    
                 </div>
             </div>
         </div>
